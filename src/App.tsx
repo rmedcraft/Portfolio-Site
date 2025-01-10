5083974f973aca02be08d254d1d21265715a18ca
rmedcraft <rwtmedcraft@gmail.com>
2025-01-10 12:25:00 -0500

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+export default function App() {
+
+    return (
+        <div className="min-h-screen bg-slate-50 dark:bg-black dark:text-white">
+            <h1 className='bg-blue-500'>HI</h1>
+        </div>
+    );
+}
+
