3e9d61b14d235936bb1c2e6dea509b9e0bc81354
rmedcraft <rowanmedcraft04@gmail.com>
2025-11-12 21:45:01 -0500

    rowan themed

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,10 @@
-import { Navbar01 } from "@/components/ui/shadcn-io/navbar-01/navbar";
+import { Navbar01, Navbar01NavLink } from "@/components/ui/shadcn-io/navbar-01/navbar";
 import { FaCode, FaCodeBranch } from "react-icons/fa"
 
 export default function Home() {
 
     return (
         <>
-            <Navbar01 logo={<FaCodeBranch />} />
         </>
     );
 }
